5d274f5a3fdc30a372bc6bffe8d9654c5abf5c84
Abhishek <abhichauhan551@gmail.com>
2018-06-02 08:31:21 +0530

    main function

--- SmartApi_main.py
+++ SmartApi_main.py
@@ -1,24 +1,12 @@
-import pyttsx
-import wikipedia
-import speech_recognition as sr
-import os
-import nltk
-import re
-import webbrowser
-import smtplib
-import requests
-import ast 
-import time 
+import SmartApi_version2
+
+## might work on controlling the objects of SmartApi_version2 from here 
 
 def main():
 	print ("working")
 
 	## for engine to convert every possible text into speech
-	engine=pyttsx.init()
-	voices = engine.getProperty('voices')
-	engine.setProperty('voice', voices[1].id)
-	engine.setProperty('rate', 150)
-	
+
 
 if __name__ == "__main__":
 	main()
